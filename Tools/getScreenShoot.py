d78bfe43da58bf8f2d29297ed2216335d3ab18c7
Alex-G-R <alex.grabski555@gmail.com>
2023-08-09 15:39:20 +0200

    Added new tool for getting screenschots of specific area, added auto upgrading castle or archers, added auto collecting of pop-up chest collectable for bonuses, general optimalisation of some functions

---
+++ Tools/getScreenShoot.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+iml = pyautogui.screenshot(region=(920, 780, 1000, 300))
+iml.save(r"C:\Users\user\Desktop\Python\GrowCastleBot\AreaScreenShot\screen.png")
+            #Put your dir here
--- app.py
+++ app.py
@@ -26,68 +26,42 @@ def start():
         checkIfBattleWon()
         if pyautogui.pixel(436, 75)[2] == 255:
             checkHero(436,75)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(559, 78)[2] == 255:
             checkHero(559,78)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(678, 76)[2] == 255:
             checkHero(678,76)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(438, 220)[2] == 255:
             checkHero(438,220)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(557, 221)[2] == 255:
             checkHero(557,221)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(676, 221)[2] == 255:
             checkHero(676,221)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(439, 363)[2] == 255:
             checkHero(439,363)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(553, 363)[2] == 255:
             checkHero(553,363)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(675, 363)[2] == 255:
             checkHero(675,363)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(680, 497)[2] == 255:
             checkHero(680,497)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(201, 721)[2] == 255:
             archerCheck(201,721)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(204, 547)[2] == 255:
             archerCheck(204,547)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
         if pyautogui.pixel(1020,28)[0] == 0:
             smithCheck(442, 555)
-            checkIfBattleWon()
-            quickRandomPause()
         checkIfBattleWon()
 
 def stop():
@@ -107,7 +81,9 @@ def checkHero(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(np.random.uniform(0.08,0.17))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-
+    if pyautogui.pixel(244,334) [1] == 116 and pyautogui.pixel(244,334) [2] == 121:
+        checkIfAdditionalClickNeeded(np.random.uniform(1705,1711),np.random.uniform(718,722))
+        
 def smithCheck (smithX,smithY):
     print("[+]Using smith")
     win32api.SetCursorPos((int(np.random.uniform(smithX - 2, smithX + 2)),int(np.random.uniform(smithY, smithY + 30))))
@@ -131,20 +107,98 @@ def checkIfAdditionalClickNeeded(clickX, clickY):
 
 def quickRandomPause():
     time.sleep(np.random.uniform(0.08,0.17))
-    if pyautogui.pixel(244,334) [1] == 116 and pyautogui.pixel(244,334) [2] == 121:
-        checkIfAdditionalClickNeeded(np.random.uniform(1705,1711),np.random.uniform(718,722))
 
 def checkIfBattleWon ():
+    autoChestCollectable()
     if pyautogui.pixel(378, 166)[0] == 45 and pyautogui.pixel(378, 166)[1] == 39 and pyautogui.pixel(378, 166)[2] == 33 or pyautogui.pixel(378, 166)[0] == 16 and pyautogui.pixel(378, 166)[1] == 14 and pyautogui.pixel(378, 166)[2] == 12:
-        print("[-]Stopping the game")
+        print("[+] Battle has ended")
+        time.sleep(4)
+        autoCastle()
+        time.sleep(1)
+        autoArchers()
+        print("Now wait for around 10 seconds for bot to turn the battle again")
         time.sleep(np.random.uniform(9,13))
         print("[+]Starting the game again")
         click(1768, 921)
         time.sleep(np.random.uniform(4.1,6))
 
+def autoCastle ():
+    global autoCastleStatus
+    if autoCastleStatus % 2 == 1:
+        if pyautogui.locateOnScreen("./images/upgradeCastle.png", confidence=0.98) != None:
+            castlex, castley = pyautogui.locateCenterOnScreen("./images/upgradeCastle.png", confidence=0.98)
+            print("Castle upgrade located. X = ", castlex, " Y = ", castley)
+            while(pyautogui.locateOnScreen("./images/noGold.png", confidence=0.98) == None):
+                win32api.SetCursorPos((int(np.random.uniform(castlex - 5, castlex + 5)),int(np.random.uniform(castley - 2, castley + 2))))
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+                time.sleep(np.random.uniform(0.08,0.17))
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+            print("[+]No more gold - stopping the auto upgrade")
+
+def autoArchers ():
+    global autoArchersStatus
+    if autoArchersStatus % 2 == 1:
+        if pyautogui.locateOnScreen("./images/upgradeArchers.png", confidence=0.98) != None:
+            archersx, archersy = pyautogui.locateCenterOnScreen("./images/upgradeArchers.png", confidence=0.98)
+            print("Archers upgrade located. X = ", archersx, " Y = ", archersy)
+            while(pyautogui.locateOnScreen("./images/noGold.png", confidence=0.98) == None):
+                win32api.SetCursorPos((int(np.random.uniform(archersx - 5, archersx + 5)),int(np.random.uniform(archersy - 2, archersy + 2))))
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+                time.sleep(np.random.uniform(0.08,0.17))
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+            print("[+]No more gold - stopping the auto upgrade")
+
+def autoChestCollectable():
+    global autoChestStatus
+    if autoChestStatus % 2 == 1:
+        chest_image_path = "./images/chest.png"
+        chest_region = (920, 780, 1000, 300)
+        confidence_threshold = 0.75
+
+        chest_location = pyautogui.locateCenterOnScreen(chest_image_path, confidence=confidence_threshold, grayscale=True, region=chest_region)
+        
+        if chest_location is not None:
+            chestx, chesty = chest_location
+            print(f"Chest collectable located. X = {chestx}, Y = {chesty}")
+            
+            click_offset_x = np.random.uniform(-2, 2)
+            click_offset_y = np.random.uniform(-2, 2)
+            win32api.SetCursorPos((int(chestx + click_offset_x), int(chesty + click_offset_y)))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+            time.sleep(np.random.uniform(0.08, 0.17))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+            
+            print("[+] Chest collected")
+
 def start_bot_thread():
     bot_thread = threading.Thread(target=start)
     bot_thread.start()
+
+autoCastleStatus = 0
+def autoCastle_thread():
+    global autoCastleStatus
+    global autoArchersStatus
+    autoCastleStatus += 1
+    autoArchersStatus = 0
+    print("[+]Auto Castle upgrade toggled, status: ", True)
+    print("[+]Auto Archers upgrade toggled, status: ", False)
+
+autoArchersStatus = 0
+def autoArchers_thread():
+    global autoArchersStatus
+    global autoCastleStatus
+    autoCastleStatus = 0
+    autoArchersStatus += 1
+    print("[+]Auto Archers upgrade toggled, status: ", True)
+    print("[+]Auto Castle upgrade toggled, status: ", False)
+
+autoChestStatus = 0
+def autoChest_thread():
+    global autoChestStatus
+    autoChestStatus +=1
+    print("[+]Auto Chest collect upgrade toggled, status: ", autoChestStatus % 2)
+
+
 #Application
 root = tk.Tk()
 root.title("Grow Castle Auto-Bot by Alex-G-R")
@@ -158,6 +212,15 @@ startBot = tk.Button(root, text="Start the Bot",command=start_bot_thread, padx=1
 canvas.create_window(195, 33, window=startBot)
 stopBot = tk.Button(root, text="Stop the Bot (hold Q to stop)",command=stop, padx=10, pady = 5, fg="black", bg="white", height=2, width=50)
 canvas.create_window(195, 468, window=stopBot)
+
+autoUpgradeCastle = tk.Button(root, text="Auto Castle upgrade", command=autoCastle_thread, fg="black", bg="white", height=2, width=25)
+canvas.create_window(100, 415, window=autoUpgradeCastle)
+
+autoUpgradeArchers = tk.Button(root, text="Auto Archers upgrade", command=autoArchers_thread, fg="black", bg="white", height=2, width=25)
+canvas.create_window(290, 415, window=autoUpgradeArchers)
+
+autoChest = tk.Button(root, text="Auto Chest collect", command=autoChest_thread, fg="black", bg="white", height=2, width=52)
+canvas.create_window(195, 367, window=autoChest)
 
 output_text = tk.Text(root, height=30, width=50)
 canvas.create_window(590, 250, window=output_text)
